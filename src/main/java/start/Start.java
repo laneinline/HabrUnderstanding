67b7757803114bd9ab84094de2487c35d8dfe05b
laneinline <44878400+laneinline@users.noreply.github.com>
2020-08-23 22:57:12 +0300

    initial commit

---
+++ src/main/java/start/Start.java
@@ -0,0 +1,10 @@
+package main.java.start;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main is working");
+	}
+
+}
